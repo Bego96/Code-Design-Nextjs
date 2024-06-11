fe41aba2e2b9e80cc187b137662f75b130c1fea4
Bego96 <begic.ismar96@gmail.com>
2024-06-11 20:26:24 +0200

    Finnished dashboard navigation

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Navigation } from "./components/navigation/navigation";
+import "@/app/globals.css";
+import { Navigation } from "../components/navigation/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-[#FAFAFA]">
-      <Navigation />
+        <Navigation />
       {children}</body>
     </html>
   );
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { auth } from "@/auth";
+import { signOut } from "@/auth";
+import { Navigation } from "../components/navigation/navigation";
+import { Test } from "../components/testComponent/test";
+
+export default function Home() {
+
+  
+  return (
+    <Test />
+  );
+}
+
