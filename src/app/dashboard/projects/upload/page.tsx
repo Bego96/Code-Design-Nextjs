b368352c9ba8e7361dd62803edc0b3eaa4099e7a
Bego96 <begic.ismar96@gmail.com>
2024-06-20 15:39:59 +0200

    Finnished rest of basic logic for form submittion

--- src/app/dashboard/projects/upload/page.tsx
+++ src/app/dashboard/projects/upload/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useRef, ChangeEvent } from 'react'
+import React, { useState, useRef, ChangeEvent, FormEvent } from 'react'
 import { FaRegTrashAlt } from "react-icons/fa";
 import { VscAdd } from "react-icons/vsc";
 import Backdrop from '@mui/material/Backdrop';
@@ -16,11 +16,14 @@ import Image from 'next/image';
 import { UploadTaskSnapshot, deleteObject, getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { db, storage } from '@/app/firebaseConfig';
 import { IoCloseSharp } from "react-icons/io5";
+import { styled } from '@mui/material/styles';
+import { OutlinedInput, makeStyles } from '@mui/material';
 
 interface ImageRef {
   id: number,
   imageSource: string,
-  imageFileRef: File
+  imageFileRef: File,
+  imageName: string
 }
 
 const style = {
@@ -28,31 +31,66 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 550,
+  width: 650,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
-  zIndex: 10
+  zIndex: 10,
+  outline: 'none'
 };
+
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 export default function UploadProjects() {
   const notify = (message: string) => toast(message);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
+  const [image, setImage] = useState<ImageRef | null>(null)
   const [imageSrc, setImageSrc] = useState<ImageRef[]>([]);
   const [progressUpload, setProgressUpload] = useState<number>(0);
 
+  const [projectName, setProjectName] = useState<string>('');
+  const [projectLocation, setProjectLocation] = useState<string>('');
+  const [projectDate, setProjectDate] = useState<Date>();
+
+  
+  const handleClose = () => {
+    if (progressUpload > 0) {
+      setProgressUpload(0);
+    }
+    
+    setOpen(false);
+  }
 
   const addImages = () => {
     // Add image to the project
+    if (image) {
+      setImageSrc(prevList => [...prevList, image]);
+      setProgressUpload(0);
+    } else {
+      notify('Please upload image!')
+    }
+    
   }
 
   const uploadImage = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       const imgObj = {
         fileRef: e.target.files[0],
-        imgSrc: URL.createObjectURL(e.target.files[0])
+        imgSrc: URL.createObjectURL(e.target.files[0]),
+        imageName: e.target.files[0].name
       };
       
 
@@ -79,25 +117,55 @@ export default function UploadProjects() {
             const imageObject = {
               id: id,
               imageSource: imageUrl,
-              imageFileRef: imgObj.fileRef
+              imageFileRef: imgObj.fileRef,
+              imageName: imgObj.imageName
             };
 
-            setImageSrc(prevList => [...prevList, imageObject]);
-            setProgressUpload(0);  // Reset progress after upload completes
+            setImage(imageObject);
+
+              // Reset progress after upload completes
           }
         );
       }
     }
   }
 
-  const uploadProject = () => {
+  const uploadProject = (event: FormEvent<HTMLFormElement>) => {
     // Function to handle project upload
+    event.preventDefault();
+    const projectInfo = {
+      projectName: projectName,
+      projectLocation: projectLocation,
+      projectDate: projectDate,
+      projectImages: imageSrc
+    }
+
+    console.log("PROJECT INFO " + JSON.stringify(projectInfo))
   }
 
-  console.log(imageSrc);
+  console.log(image?.imageFileRef);
   console.log(progressUpload);
 
+  const deleteUploadedImg = async (imgName: File) => {
+    if (image) {
+      const storage = getStorage();
+        console.log(imgName)
+        const desertRef = ref(storage, `images/${imgName.name}`);
+
+        deleteObject(desertRef).then(() => {
+          console.log("Image removed successfuly")
+          setImage(null);
+          setProgressUpload(0);
+        }).catch((error) => {
+          console.log("Error occured deleting image!")
+        });
+
+
+    }
+  }
+
   const deleteImage = async (imgName: File) => {
+    console.log("IM CLICKED")
     for (let i = 0; i < imageSrc.length; i++) {
       if (imageSrc[i].imageFileRef === imgName) {
         const storage = getStorage();
@@ -112,12 +180,13 @@ export default function UploadProjects() {
 
         const newArray = imageSrc.filter((item) => item !== imageSrc[i]);
         setImageSrc(newArray);
+        
       }
     }
   }
 
   console.log(imageSrc[0]?.imageSource)
-
+  
   return (
     <div className='mt-20'>
       {/* Modal window for image upload */}
@@ -139,7 +208,7 @@ export default function UploadProjects() {
             <Typography id="transition-modal-title" className="text-center" variant="h6" component="h2">
               <div>
                 <div className="flex items-center justify-center w-full">
-                  <label htmlFor="dropzone-file" className="flex items-center justify-center w-full p-6 border-2 border-dashed rounded-lg cursor-pointer border-[#495057]">
+                  <label htmlFor="dropzone-file" className="flex items-center justify-around w-full p-6 border-2 border-dashed rounded-lg cursor-pointer border-[#495057]">
                     <div className="flex items-center justify-center pt-5 pb-6">
                       <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
@@ -150,13 +219,36 @@ export default function UploadProjects() {
                       </div>
                     </div>
                     <input id="dropzone-file" onChange={uploadImage} type="file" className="hidden" />
+                    <Button
+                      component="label"
+                      role={undefined}
+                      variant="contained"
+                      tabIndex={-1}
+                      
+                     sx={{
+                      backgroundColor: '#222222'
+                     }}
+                      >
+                       Upload file
+                      <VisuallyHiddenInput type="file" />
+                    </Button>
                   </label>
+                  
                 </div>
+                
               </div>
             </Typography>
             <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              
                   {progressUpload > 0 && (
                 <div className='mt-4 w-full'>
+                      {
+                    image && <div className='flex justify-between'>
+                      <p>{image?.imageName}</p>
+                      <IoCloseSharp onClick={() => deleteUploadedImg(image?.imageFileRef)} size={25} color='#495057' className='bg-slate-200 bg-opacity-60 cursor-pointer' />
+                  
+                  </div>
+                  }
                   <div className='h-2 bg-gray-300'>
                     <div className='h-full bg-blue-600' style={{ width: `${progressUpload}%` }}></div>
                   </div>
@@ -165,7 +257,7 @@ export default function UploadProjects() {
               )}
               <div className="flex justify-between mt-10">
                 <button onClick={handleClose} className='w-[45%] py-2 bg-[#4F6071] text-white'>Odustani</button>
-                <button onClick={addImages} className='w-[45%] py-2 bg-[#222222] text-white'>Dodaj</button>
+                <button onClick={addImages} className='w-[45%] py-2 bg-gray-400 text-white'>Dodaj</button>
               </div>
             </Typography>
           </Box>
@@ -178,16 +270,16 @@ export default function UploadProjects() {
           <h2 className='text-3xl text-[#495057] mb-4'>NOVI PROJEKAT</h2>
           <div className='flex flex-col xsm:flex-row'>
             <Link href="/dashboard/projects"><button className='bg-[#4F6071] w-[200px] py-2 mb-4 xsm:mb-0 xsm:mr-4 text-white text-center'>Odustani</button></Link>
-            <Link href="/dashboard/projects/upload"><button type="submit" className='bg-gray-400 w-[200px] py-2 text-white text-center'>Snimi projekat</button></Link>
+            <button type="submit" className='bg-gray-400 w-[200px] py-2 text-white text-center'>Snimi projekat</button>
           </div>
         </div>
 
         {/* Form with input fields */}
         <div className='w-full md:w-1/2 mt-10'>
-          <input type="text" placeholder='Naziv' className='border border-[#495057] h-[48px] placeholder-[#495057] pl-4 w-full'/>
+          <input type="text" placeholder='Naziv' value={projectName} onChange={(e) => setProjectName(e.target.value)} className='border border-[#495057] h-[48px] placeholder-[#495057] pl-4 w-full'/>
           <div className='flex flex-col sm:flex-row mt-4'>
-            <input type="text" placeholder='Lokacija' className='border border-[#495057] h-[48px] placeholder-[#495057] pl-4 w-full sm:w-[70%] mr-auto' />
-            <input type="date" placeholder="Datum" className='mt-4 sm:mt-0 border border-[#495057] h-[48px] placeholder-[#495057] pl-4 w-full sm:w-[28%]'/>
+            <input type="text" placeholder='Lokacija' value={projectLocation} onChange={(e) => setProjectLocation(e.target.value)} className='border border-[#495057] h-[48px] placeholder-[#495057] pl-4 w-full sm:w-[70%] mr-auto' />
+            <input type="date" placeholder="Datum" onChange={(e) => setProjectDate(new Date(e.target.value))} className='mt-4 sm:mt-0 border border-[#495057] h-[48px] placeholder-[#495057] pl-4 w-full sm:w-[28%]'/>
           </div>
         </div>
       </form>
@@ -199,7 +291,7 @@ export default function UploadProjects() {
           <div className='flex flex-wrap'>
             {imageSrc.length > 0 && (
               imageSrc.map((image) => (
-                <div key={image.id}  className={`relative w-[200px] h-[200px] ${image.id === 1 ? 'ml-0' : 'ml-4'}`} onClick={() => deleteImage(image.imageFileRef)}>
+                <div key={image.id}  className={`relative w-[200px] h-[200px] ${image.id === 1 ? 'ml-0' : 'ml-4'}`}>
                 <IoCloseSharp onClick={() => deleteImage(image.imageFileRef)} size={25} color='#495057' className='bg-slate-200 bg-opacity-60 cursor-pointer absolute top-2 right-2' />
                   
                 
