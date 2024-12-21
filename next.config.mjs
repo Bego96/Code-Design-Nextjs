b89c1c722169e3990114468f4fb4c7610e5340d7
Bego96 <begic.ismar96@gmail.com>
2024-12-21 13:19:17 +0100

    Fixes towards securing env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+    FBASE_AUTH_DOMAIN: process.env.FBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    FBASE_MESSAGING_SENDER_ID: process.env.FBASE_MESSAGING_SENDER_ID,
+    FBASE_APP_ID: process.env.FBASE_APP_ID,
+    FBASE_MEASUREMENT_ID: process.env.FBASE_MEASUREMENT_ID,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY
+  },
   images: {
       unoptimized: true,
       remotePatterns: [
--- src/app/firebaseConfig.tsx
+++ src/app/firebaseConfig.tsx
@@ -4,6 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,17 +12,26 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDV00MBt7yFJMPruHl2as5SRo4qz3usbAY",
-  authDomain: "code-design-36e78.firebaseapp.com",
-  projectId: "code-design-36e78",
-  storageBucket: "code-design-36e78.appspot.com",
-  messagingSenderId: "441819669107",
-  appId: "1:441819669107:web:82b8d03bda2ec1ed8f0006",
-  measurementId: "G-T8BQ1CF97M"
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FBASE_APP_ID,
+  measurementId: process.env.FBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);;
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+/*const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('abcdefghijklmnopqrstuvwxy-1234567890abcd'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});*/
+
 const db = getFirestore(app)
 const storage = getStorage(app)
 const auth = getAuth(app);
